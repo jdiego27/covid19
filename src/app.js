1dcf838894a54d5e31a480bd9b9be4e8cf3466f8
JosymarM <josymar.mayhua@gmail.com>
2020-07-14 17:38:36 -0500

    Creación de páginas

--- src/app.js
+++ src/app.js
@@ -8,12 +8,12 @@ const engine = require('ejs');
 const app = express();
 
 // Importing routes
-const indexRoutes = require('../routes/index');
+const indexRoutes = require('./routes/index');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 // Middleware
